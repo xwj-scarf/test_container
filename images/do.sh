741b3a37c836bfa3eff6d9724cfeca93f6b0d086
xwj-scarf <934098774@qq.com>
2018-02-05 23:19:45 +0800

    1

--- images/do.sh
+++ images/do.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 chmod 777 /tmp/in.txt
+
+begin_time=$(date +%s%N)
 (while read LINE
 do 
 ./code << EOF 
@@ -8,4 +10,9 @@ do
 EOF
 done  < /tmp/in.txt
 ) > /tmp/out.txt
+
+end_time=$(date +%s%N)
+dif=$((end_time - begin_time))
+use_time=$((dif / 1000000))
+echo $use_time
 
